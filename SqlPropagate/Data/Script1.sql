5165271456401d7817fdd89d7f69d7899b85a607
Evaldas Jocys <evaldas@jocys.com>
2021-09-16 16:13:27 +0100

    Help updated.

--- SqlPropagate/Data/Script1.sql
+++ SqlPropagate/Data/Script1.sql
@@ -1,4 +1,11 @@
-﻿SELECT @@VERSION
+﻿USE [master]
+GO
 
-DECLARE @error sysname = 'Test Message from Script 1. MyParam1 = ''$(MyParam1)'''
+DECLARE @error sysname
+SELECT @error = 'Server Language = ' + @@LANGUAGE
+RAISERROR(@error, -1, -1) WITH NOWAIT
+
+GO
+DECLARE @error sysname
+SELECT @error = 'Message from Script 1. MyParam1 = ''$(MyParam1)''.'
 RAISERROR(@error, -1, -1) WITH NOWAIT
--- SqlPropagate/Data/Script2.sql
+++ SqlPropagate/Data/Script2.sql
@@ -1,4 +1,2 @@
-﻿SELECT @@VERSION
-
-DECLARE @error sysname = 'Test Message from Script 2. MyParam1 = ''$(MyParam1)'''
+﻿DECLARE @error sysname = 'Test Message from Script 2. MyParam1 = ''$(MyParam1)'''
 RAISERROR(@error, -1, -1) WITH NOWAIT
