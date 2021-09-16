120e20ea6a4118b9cba85fb2a09ec0102a000885
Evaldas Jocys <evaldas@jocys.com>
2021-09-16 16:16:41 +0100

    Help updated.

--- SqlPropagate/Data/Script1.sql
+++ SqlPropagate/Data/Script1.sql
@@ -6,6 +6,5 @@ SELECT @error = 'Server Language = ' + @@LANGUAGE
 RAISERROR(@error, -1, -1) WITH NOWAIT
 GO
 
-DECLARE @error sysname
-SELECT @error = 'Message from Script 1. MyParam1 = ''$(MyParam1)''.'
+DECLARE @error sysname = 'Message from Script 1. MyParam1 = ''$(MyParam1)''.'
 RAISERROR(@error, -1, -1) WITH NOWAIT
